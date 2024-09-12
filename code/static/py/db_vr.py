de960c7832e9fd8e8da8f94a49cc92dfd0042f14
João Guilherme <joaogfp07@gmail.com>
2024-09-12 10:19:50 -0300

    Excluindo db_vr

--- code/static/py/db_vr.py
+++ code/static/py/db_vr.py
@@ -1,16 +1,1 @@
-# static/py/db.py
-import psycopg2
-
-# Database connection configuration
-DB_CONFIG = {
-    'dbname': 'vr_bkp',
-    'user': 'postgres',
-    'password': 'VrPost@Server',
-    'host': '192.168.1.237'
-}
-
-def get_db_vr():
-    """Establish a connection to the database."""
-    conn = psycopg2.connect(**DB_CONFIG)
-    return conn
 
